985476d5bb6018612a0d6ba0178df49f0d6421be
HamFeet <j.a.feetham@gmail.com>
2025-11-11 18:51:07 +1300

    Refactor: Added changed files from main merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 import Header from './common/Header/Header'
-=======
-import { useState } from 'react'
-import Header from '../src/common/Header/Header'
-
->>>>>>> main
 import './App.css'
 import './styles/base.css'
 
