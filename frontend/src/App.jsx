876b1b42c23b7273f7b8157f502d6f26b911bd08
TomasTF <ronyttf@missionreadyhq.com>
2025-11-11 18:38:08 +1300

    Update App.jsx
    Delete the Footer from the App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Header from '../src/common/Header/Header'
-import Footer from '../src/common/Footer/Footer'
+
 import './App.css'
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
     
     </main>
 
-    <Footer/>
+ 
 
     </>
   )
