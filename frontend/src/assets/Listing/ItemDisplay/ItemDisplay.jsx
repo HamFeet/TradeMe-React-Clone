7a2939edabb9f34565f39782cc0684419f3f526f
HamFeet <j.a.feetham@gmail.com>
2025-11-22 19:04:57 +1300

    Refactor: Changed styling to better reflect the design

--- frontend/src/assets/Listing/ItemDisplay/ItemDisplay.jsx
+++ frontend/src/assets/Listing/ItemDisplay/ItemDisplay.jsx
@@ -4,25 +4,25 @@ import React from 'react'
 import styles from "./ItemDisplay.module.css"
 
 
-function ItemDisplay({listing}) {
+function ItemDisplay({ item }) {
   return (
     <div className={styles.content}>
         <div className={styles.container}>
             <div className={styles.mainImage}>
-                <img src={listing.item.imagePath} alt={`A image of ${listing.item.title}`} />
+                <img src={item.imagePath} alt={`A image of ${item.title}`} />
             </div>
             <div className={styles.gallery}>
-                <img src={listing.item.imagePath} alt={`A image of ${listing.imageDescription}`} />
-                <img src={listing.item.imagePath} alt={`A image of ${listing.imageDescription}`} />
-                <img src={listing.item.imagePath} alt={`A image of ${listing.imageDescription}`} />
-                <img src={listing.item.imagePath} alt={`A image of ${listing.imageDescription}`} />
-                <img src={listing.item.imagePath} alt={`A image of ${listing.imageDescription}`} />
+                <img src={item.imagePath} alt={`A image of ${item.imageDescription}`} />
+                <img src={item.imagePath} alt={`A image of ${item.imageDescription}`} />
+                <img src={item.imagePath} alt={`A image of ${item.imageDescription}`} />
+                <img src={item.imagePath} alt={`A image of ${item.imageDescription}`} />
+                <img src={item.imagePath} alt={`A image of ${item.imageDescription}`} />
             </div>
             <div className={styles.description}>
                 <h3>Details</h3>
-                <p>{`Condition: ${listing.item.condition}`}</p>
+                <p>{`Condition: ${item.condition}`}</p>
                 <h3>Full Description</h3>
-                <p>{listing.item.itemDescription}</p>
+                <p>{item.itemDescription}</p>
             </div>
         </div>
     </div>
--- frontend/src/pages/Listing/Listing.jsx
+++ frontend/src/pages/Listing/Listing.jsx
@@ -1,19 +1,27 @@
 import ListingDescriptionStack from "../../common/ListingDescriptionStack/ListingDescriptionStack.jsx";
-import ItemDisplay from '../../assets/Listing/ItemDisplay/ItemDisplay.jsx';
+import ItemDisplay from "../../assets/Listing/ItemDisplay/ItemDisplay.jsx";
 import QuestionAndAnswer from "../../assets/Listing/QuestionAndAnswer/QuestionAndAnswer.jsx";
 import AboutTheSeller from "../../assets/Listing/AboutTheSeller/AboutTheSeller.jsx";
 
-import styles from "./Listing.module.css"
+import styles from "./Listing.module.css";
 
-function Listing(item) {
+function Listing({ item }) {
   return (
     <div className={styles.container}>
-      <ListingDescriptionStack listing={item} />
-      <ItemDisplay listing={item}/>
-      <QuestionAndAnswer/>
-      <AboutTheSeller/>
+
+      <div className={styles.leftColumn}>
+        <ItemDisplay item={item} />
+        <QuestionAndAnswer />
+        <AboutTheSeller />
+      </div>
+
+      <div className={styles.rightColumn}>
+        {/* This should contain the watchlist button + bidding box */}
+        <ListingDescriptionStack item={item} sidebar />
+      </div>
+
     </div>
-  )
+  );
 }
 
 export default Listing;
